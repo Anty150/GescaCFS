28ab3726ece820b10c658eab245e2a977e18a6ff
HubertRudowicz <rudowicyhubert@gmail.com>
2023-06-01 10:51:45 +0200

    added addToDB.php file

---
+++ addToDB.php
@@ -0,0 +1,22 @@
+<?php
+/*$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Gesca";
+
+$connect = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($connect->connect_error) {
+    die("Connection failed: " . $connect->connect_error);
+}
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('', '', '')";//dokonczyc
+
+if ($connect->query($sql) === FALSE) {
+    echo "Error: " . $sql . "<br>" . $connect->error;
+}
+
+$connect->close();
+*/
+?>
+<body onload="history.go(-1);">
